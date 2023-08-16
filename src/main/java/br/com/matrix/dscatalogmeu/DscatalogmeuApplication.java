5b871da1f00b447a1535e63ccf86029d19693788
jandirlei <jandirlei@yahoo.com.br>
2023-08-15 22:36:13 -0300

    Backed project created

---
+++ src/main/java/br/com/matrix/dscatalogmeu/DscatalogmeuApplication.java
@@ -0,0 +1,13 @@
+package br.com.matrix.dscatalogmeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogmeuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogmeuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matrix/dscatalogmeu/DscatalogmeuApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matrix.dscatalogmeu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogmeuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
